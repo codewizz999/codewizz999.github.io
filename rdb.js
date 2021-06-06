12d47a3d34d1532034d3ffbc272d0356fb6a0d0c
Pedros Balandinos <68953160+codewizz999@users.noreply.github.com>
2021-06-06 14:31:29 +0200

    Update rdb.js

--- rdb.js
+++ rdb.js
@@ -2,5 +2,5 @@ Const fs = require('fs');
 
 fs.readFile('package.json', (err, data) => {                                   27     if (err) throw err;
 let studentone = JSON.parse(data);
-console.log(studentone);
+var httpRead = document.getElementById("node").innerHTML =studentone;
 });
