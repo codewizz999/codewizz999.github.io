c67a29abb952b62724df8c5a011dcc5d3bccc79d
Pedros Balandinos <68953160+codewizz999@users.noreply.github.com>
2021-06-06 14:37:13 +0200

    Update rdb.js

--- rdb.js
+++ rdb.js
@@ -1,6 +1,6 @@
 Const fs = require('fs');
 
-fs.readFile('./package.json', (err, data) => {                                   27     if (err) throw err;
+fs.readFile('https://codewizz999.github.io/package.json', (err, data) => {                                   27     if (err) throw err;
 let studentone = JSON.parse(data);
 var httpRead = document.getElementById("node").innerHTML =studentone;
 });
