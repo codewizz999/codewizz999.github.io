979eb4872596e6f57a24a2a94768fdf00c622d3a
Pedros Balandinos <68953160+codewizz999@users.noreply.github.com>
2021-06-10 13:24:34 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function opennav(){
+document.getElementbyId("navs").style.width="250px";
+document.getElementById("content").style.marginLeft="250px";
+}
+function closenav(){
+document.getElementbyId("navs").style.display="none";
+document.getElementbyId("content").style.marginLeft="0px";
+
+
+}
