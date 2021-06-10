403a7f2b852544def93e2fb7aed90c6302d6c4e6
Pedros Balandinos <68953160+codewizz999@users.noreply.github.com>
2021-06-10 16:26:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-
-/*function closenav(){
-document.getElementbyId("navs").style.display="none";
-document.getElementbyId("content").style.marginLeft="0px";
-
-
-}*/
 function closenav(){
   var x = document.getElementById("side-nav");
-  var y = document.getElementById("content");
   if (x.style.width === "none") {
   
       x.style.display="block";
@@ -16,11 +8,10 @@ function closenav(){
       x.style.display="none";
     
     }
-alert("close");
 }
 function opennav(){
   var x = document.getElementById("side-nav");
-  var y = document.getElementById("content");
+  
   if (x.style.width === "none") {
   
       x.style.display="block";
@@ -29,5 +20,5 @@ function opennav(){
       x.style.display="none";
     
     }
-alert("open");
+
 }
