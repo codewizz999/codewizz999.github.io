4326368c35c66982e761ce17cef11e16cc5ea51a
Pedros Balandinos <68953160+codewizz999@users.noreply.github.com>
2021-06-10 16:19:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function closenav(){
   var y = document.getElementById("content");
   if (x.style.width === "none") {
   
-      x.style.display="show";
+      x.style.display="block";
       
     } else {
       x.style.display="none";
@@ -23,7 +23,7 @@ function opennav(){
   var y = document.getElementById("content");
   if (x.style.width === "none") {
   
-      x.style.display="show";
+      x.style.display="block";
       
     } else {
       x.style.display="none";
