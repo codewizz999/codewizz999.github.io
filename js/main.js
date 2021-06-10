6e0281e82ce9135e29d417017489907a3d230465
Pedros Balandinos <68953160+codewizz999@users.noreply.github.com>
2021-06-10 14:05:46 +0200

    Update and rename main.js to js/main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 
-function opennav(){
-if(
-document.getElementbyId("navs").style.width="250px";
-document.getElementById("content").style.marginLeft="250px";
-}
 /*function closenav(){
 document.getElementbyId("navs").style.display="none";
 document.getElementbyId("content").style.marginLeft="0px";
