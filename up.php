e2e3b402821f991eefedb092c16cc320cadca1bd
Pedros Balandinos <68953160+codewizz999@users.noreply.github.com>
2021-07-18 10:16:31 +0200

    Update up.php

--- up.php
+++ up.php
@@ -1,18 +1,16 @@
 <?php      
-  $myPost = json_decode('somejson',true);
+  $myPost = json_decode('website-data/readonly.json',true);
  $myPostId= 1;
 
 foreach($myPost as &$e){
-if($e['id'] == $myPostId){
-if($e['post'] =="naah"){
-$e['post'] == "amos";
+
+if($e[0]['post'] =="naah"){
+$e[0]['post'] == "amos";
 }
-if($e['id'] == "yaah"){
-if($e['post'] =="yaah"){
-$e['id'] == 3;
-}
-}
+if($e[0]['post'] =="amos"){
+$e[1]['post'] == "yaayy awesome";
+
 }
 $newPostData = json_encode($myPostId, JSON_PRETTY_PRINT);
-echo file_put_contents('somejson',$newPostData);
+echo file_put_contents('website-data/readonly.json',$newPostData);
   ?>
